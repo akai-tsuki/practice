4aa7988e067325bb6de2b77512b1a91d307c628b
akai-tsuki <akai.tsuki.mobile@gmail.com>
2016-05-07 20:59:09 +0900

    [edit]set permission

--- tomcat_container/init.sh
+++ tomcat_container/init.sh
@@ -6,8 +6,9 @@ docker run -it -d --name ${CONTAINER} -p 8888:8080 -p 2222:22 ${IMAGE} bash
 
 ssh-keygen -t rsa -f root_ssh_rsa_key -N ""
 mv root_ssh_rsa_key.pub authorized_keys
-chmod 600 authorized_keys
 docker cp authorized_keys ${CONTAINER}:/root/.ssh/authorized_keys
+docker exec ${CONTAINER} chmod 600 /root/.ssh/authorized_keys
 
 ### ssh exsample
+# docker exec -d ${CONTAINER} /usr/sbin/sshd -D
 # ssh root@172.17.0.2 -i root_ssh_rsa_key
